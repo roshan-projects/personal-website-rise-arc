e70c418542b1cc54c81d7d518248550d6ebaa873
roshan-projects <jameelroshan23@gmail.com>
2026-01-25 16:01:00 +0530

    Load hamburger menu JS on all pages

--- script.js
+++ script.js
@@ -373,3 +373,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navbarMenu = document.querySelector(".navbar-menu");
+
+  if (menuToggle && navbarMenu) {
+    menuToggle.addEventListener("click", function () {
+      navbarMenu.classList.toggle("active");
+    });
+  }
+});
