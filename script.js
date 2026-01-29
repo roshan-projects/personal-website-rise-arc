626e6f05c8c3b4473b18d253a311af103ee95426
roshan-projects <jameelroshan23@gmail.com>
2026-01-29 21:32:17 +0530

    Updated navbar mobile fix

--- script.js
+++ script.js
@@ -372,4 +372,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("navbarMenu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
 
